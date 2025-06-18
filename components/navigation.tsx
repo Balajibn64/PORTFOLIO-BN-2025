52706fcee41ccceda596e08843e037ccdb08d285
balajibn64 <balajibn6464.com>
2025-06-19 00:31:58 +0530

    Final Commit

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -87,14 +87,14 @@ export function Navigation() {
 
           {/* Mobile Navigation */}
           <div className="md:hidden flex items-center space-x-4">
-            <Button
+            {/* <Button
               variant="ghost"
               size="icon"
               onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
             >
               <Sun className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
               <Moon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-            </Button>
+            </Button> */}
             <Button
               variant="ghost"
               size="icon"
